1915f38653dc2f160fc99a2d8f14db63613564c1
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2016-12-03 14:31:21 +0100

    Module 5 finished.

--- DavidChu'sChinaBistro/js/script.js
+++ DavidChu'sChinaBistro/js/script.js
@@ -1,1 +1,11 @@
 // JavaScript source code
+$(function() {
+
+    $("#navbarToggle").blur(function (event) {
+        var screenWidth = window.innerWidth;
+        if (screenWidth < 768) {
+            $("#collapsable-nav").collapse("hide");
+        }
+    });
+
+});
